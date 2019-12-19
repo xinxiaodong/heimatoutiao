ec868084e6dab4c6d34708d474b7759e3a611e61
xxd <826795224@qq.com>
2019-12-19 22:33:41 +0800

    主页模块-头部信息查询

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -6,10 +6,10 @@
         </el-col>
         <el-col class="right" :sapn="4">
             <el-row type='flex' justify="end" align="middle">
-                <img src="../../assets/img/305222.jpg" alt="">
+                <img :src="!userInfo.photo ? userInfo.photo : defaultImg" alt="">
                 <!-- 下拉菜单 -->
                 <el-dropdown>
-                    <span>美女如画</span>
+                    <span>{{userInfo.name}}</span>
                     <!-- 下拉菜单  具名插槽 -->
                     <el-dropdown-menu slot="dropdown">
                         <!-- 下拉内容 -->
@@ -25,6 +25,24 @@
 
 <script>
 export default {
+  data () {
+    return {
+      userInfo: {}, // 用户信息
+      defaultImg: require('../../assets/img/305222.jpg') // 先把地址转换称变量
+    }
+  },
+  created () {
+    let token = window.localStorage.getItem('user-token') // 获取令牌
+    // 查询数据
+    this.$axios({
+      url: '/user/profile',
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(result => {
+      this.userInfo = result.data.data // 获取用户个人信息
+    })
+  }
 }
 </script>
 <style scoped>
