8863acef382688f489fcc6a0b6f05432014339b4
xxd <826795224@qq.com>
2019-12-19 19:07:55 +0800

    主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,18 @@
 <template>
-    <div class="login">
-        <span>主页</span>
-    </div>
+   <el-container>
+       <!-- 左右布局 -->
+    <el-aside>左侧</el-aside>
+       <!-- 放置container -->
+       <el-container>
+      <!-- 上下布局 -->
+      <el-header>
+          头部
+      </el-header>
+      <el-main>
+          主体
+      </el-main>
+       </el-container>
+   </el-container>
 </template>
 
 <script>
