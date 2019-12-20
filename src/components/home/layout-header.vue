9df0b101c058a1c4ea08f836baca4e7cdeca57dd
xxd <826795224@qq.com>
2019-12-20 14:47:32 +0800

    主页模块-头部退出系统

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -1,26 +1,26 @@
 <template>
-    <el-row class='layout-header' type="flex" align="middle" justify="space-between">
-        <el-col class="left" :sapn="6">
-            <i class='el-icon-s-unfold'></i>
-            <span class='title'>江苏传智播客教育科技股份有限公司</span>
-        </el-col>
-        <el-col class="right" :sapn="4">
-            <el-row type='flex' justify="end" align="middle">
-                <img :src="!userInfo.photo ? userInfo.photo : defaultImg" alt="">
-                <!-- 下拉菜单 -->
-                <el-dropdown>
-                    <span>{{userInfo.name}}</span>
-                    <!-- 下拉菜单  具名插槽 -->
-                    <el-dropdown-menu slot="dropdown">
-                        <!-- 下拉内容 -->
-                        <el-dropdown-item>个人信息</el-dropdown-item>
-                        <el-dropdown-item>Git地址</el-dropdown-item>
-                        <el-dropdown-item>退出</el-dropdown-item>
-                    </el-dropdown-menu>
-                </el-dropdown>
-            </el-row>
-        </el-col>
-    </el-row>
+  <el-row class='layout-header' type="flex" align="middle" justify="space-between">
+    <el-col class="left" :sapn="6">
+      <i class='el-icon-s-unfold'></i>
+      <span class='title'>江苏传智播客教育科技股份有限公司</span>
+    </el-col>
+    <el-col class="right" :sapn="4">
+      <el-row type='flex' justify="end" align="middle">
+        <img :src="!userInfo.photo ? userInfo.photo : defaultImg" alt="">
+        <!-- 下拉菜单 -->
+        <el-dropdown @command="handle">
+          <span>{{userInfo.name}}</span>
+          <!-- 下拉菜单  具名插槽 -->
+          <el-dropdown-menu slot="dropdown">
+            <!-- 下拉内容 -->
+            <el-dropdown-item command="info">个人信息</el-dropdown-item>
+            <el-dropdown-item command="git">Git地址</el-dropdown-item>
+            <el-dropdown-item command="lgout">退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-row>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -42,27 +42,35 @@ export default {
     }).then(result => {
       this.userInfo = result.data.data // 获取用户个人信息
     })
+  },
+  methods: {
+    handle (command) {
+      if (command === 'lgout') {
+        window.localStorage.removeItem('user-token') // 删除用户的令牌
+        this.$router.push('/login')
+      }
+    }
   }
 }
 </script>
 <style scoped>
-    .layout-header {
-        height: 60px;
-    }
+  .layout-header {
+    height: 60px;
+  }
 
-    .layout-header .left {
-        font-size: 18px
-    }
+  .layout-header .left {
+    font-size: 18px
+  }
 
-    .left .title {
-        margin-left: 4px;
-        color: #2c3e50;
-        font-size: 16px;
-    }
+  .left .title {
+    margin-left: 4px;
+    color: #2c3e50;
+    font-size: 16px;
+  }
 
-    .right img {
-        width: 40px;
-        height: 40px;
-        border-radius: 50%;
-    }
+  .right img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
 </style>
