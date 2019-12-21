f035b004114c6c85271ad2eb24036288471d327f
xxd <826795224@qq.com>
2019-12-21 21:44:24 +0800

    评论列表-评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,24 +1,60 @@
 <template>
     <!-- 卡片组件 -->
 
-<el-card>
-  <bread-crumb slot="header">
-    <!-- 插槽内容 -->
-    <template slot="title">评论列表</template>
-</bread-crumb>
-<el-table>
-    <!-- 列组件 -->
-    <el-table-column width="600" label="标题"></el-table-column>
-    <el-table-column label="评论状态"></el-table-column>
-    <el-table-column label="总评论数"></el-table-column>
-    <el-table-column label="粉丝评论数"></el-table-column>
-    <el-table-column label="操作"></el-table-column>
-</el-table>
-</el-card>
+    <el-card>
+        <bread-crumb slot="header">
+            <!-- 插槽内容 -->
+            <template slot="title">评论列表</template>
+        </bread-crumb>
+        <el-table :data="list">
+            <!-- 列组件 -->
+            <el-table-column prop="title" width="600" label="标题"></el-table-column>
+            <el-table-column :formatter="formatterBool" prop="comment_status" label="评论状态"></el-table-column>
+            <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+            <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+            <el-table-column label="操作">
+                <template slot-scope="obj">
+                    <el-button type="text" size="small">修改</el-button>
+                    <el-button type="text" size="small">{{ obj.row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
+                </template>
+
+            </el-table-column>
+
+        </el-table>
+    </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    //   请求评论列表数据
+    getComment () {
+      // axios 默认是get类型
+      this.$axios({
+        url: '/articles',
+        params: { response_type: 'comment' }
+      }).then(result => {
+        this.list = result.data.results // 获取评论数据给本身data
+      })
+    },
+    // 定义一个布尔值转化方法
+    formatterBool (row, column, cellValue, index) {
+      // row 当前行数
+      // column 当前列属性
+      // cellValue 当前单元格的值
+      // index 当前下标
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    // 调用请求数据方法
+    this.getComment()
+  }
 }
 </script>
 <style>
