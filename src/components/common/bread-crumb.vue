d1100b1774da50517b979a1c7229214df51c894f
xxd <826795224@qq.com>
2019-12-21 19:42:59 +0800

    面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,17 @@
+<template>
+<!-- 面包屑内容 -->
+<el-breadcrumb>
+    <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+    <el-breadcrumb-item>
+         <!-- 插槽 -->
+         <slot name="title"></slot>
+    </el-breadcrumb-item>
+</el-breadcrumb>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style>
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import layoutAside from './home/layout-aside.vue'
 import layoutHeader from './home/layout-header.vue'
+import BreadCrumb from './common/bread-crumb.vue'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside) // 全局注册
     Vue.component('layout-header', layoutHeader) // 全局注册
+    Vue.component('bread-crumb', BreadCrumb) // 全局注册面包屑组件
   }
 }
