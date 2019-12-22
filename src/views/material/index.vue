756b274ae0e251b9861ae4abf60740f26f4a562d
xxd <826795224@qq.com>
2019-12-22 21:32:17 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -4,11 +4,25 @@
     <bread-crumb slot="header">
     <template slot="title">素材管理</template>
 </bread-crumb>
+<!-- 素材 -->
+<el-tabs v-model="activeName">
+    <el-tab-pane label="全部" name="all"></el-tab-pane>
+    <el-tab-pane label="收藏" name="collect"></el-tab-pane>
+</el-tabs>
+
 </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      activeName: 'collect'
+    }
+  },
+  methods: {
+
+  }
 }
 </script>
 <style>
