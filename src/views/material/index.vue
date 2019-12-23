95324c6f4af66896d3a610c2cfda1b483e734f56
xxd <826795224@qq.com>
2019-12-23 17:57:09 +0800

    素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,35 +1,39 @@
 <template>
-    <!-- 基本的页面结构 -->
-    <el-card>
-        <bread-crumb slot="header">
-            <template slot="title">素材管理</template>
-        </bread-crumb>
-        <!-- 素材 -->
-        <el-tabs v-model="activeName" @tab-click="changeTab">
-            <el-tab-pane label="全部" name="all">
-                <!-- 全部素材内容 -->
-                <div class="img-list">
-                    <el-card class="img-card" v-for="item in list" :key="item.id">
-                        <img :src="item.url" alt="">
-                        <el-row class="operate" type="flex" align="middle" justify="space-around">
-                            <i class="el-icon-star-on"></i>
-                            <i class="el-icon-delete-solid"></i>
-                        </el-row>
-                    </el-card>
-                </div>
-            </el-tab-pane>
-            <el-tab-pane label="收藏" name="collect">
-                <!-- 收藏素材内容 -->
-                <div class="img-list">
-                    <el-card class="img-card" v-for="item in list" :key="item.id">
-                        <img :src="item.url" alt="">
+  <!-- 基本的页面结构 -->
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <!-- 素材 -->
+    <el-tabs v-model="activeName" @tab-click="changeTab">
+      <el-tab-pane label="全部" name="all">
+        <!-- 全部素材内容 -->
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt="">
+            <el-row class="operate" type="flex" align="middle" justify="space-around">
+              <i class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
+            </el-row>
+          </el-card>
+        </div>
+        <el-row type="flex" justify="center" style="height: 80px;" align="middle">
+          <el-pagination background layout="prev,pager,next" :total="page.total" :current-page="page.currentPage"
+            :page-size="page.pageSize" @current-change="changePage"></el-pagination>
+        </el-row>
+      </el-tab-pane>
+      <el-tab-pane label="收藏" name="collect">
+        <!-- 收藏素材内容 -->
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt="">
 
-                    </el-card>
-                </div>
-            </el-tab-pane>
-        </el-tabs>
+          </el-card>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
 
-    </el-card>
+  </el-card>
 </template>
 
 <script>
@@ -37,22 +41,33 @@ export default {
   data () {
     return {
       activeName: 'all', // 默认选中全部
-      list: [] // 接受全部数据
-
+      list: [], // 接受全部数据
+      page: {
+        currentPage: 1, // 当前页码
+        pageSize: 8, // 每页条数
+        total: 0 // 总条数
+      }
     }
   },
   methods: {
+    //  切换分页
+    changePage (newPage) {
+      this.page.currentPage = newPage // 得到最新页码
+      this.getAllMaterial()
+    },
     // 切换tab事件
     changeTab () {
+      this.page.currentPage = 1
       this.getAllMaterial()
     },
     // 获取所有素材
     getAllMaterial () {
       this.$axios({
         url: '/user/images',
-        params: { collect: this.activeName === 'collect' }
+        params: { collect: this.activeName === 'collect', page: this.page.currentPage, per_page: this.page.pageSize }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
   },
@@ -62,30 +77,30 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-    .img-list {
-        display: flex;
-        flex-wrap: wrap;
-    }
+  .img-list {
+    display: flex;
+    flex-wrap: wrap;
+  }
 
-    .img-list .img-card {
-        width: 200px;
-        height: 240px;
-        margin: 20px 50px;
-        position: relative;
-    }
+  .img-list .img-card {
+    width: 200px;
+    height: 240px;
+    margin: 20px 50px;
+    position: relative;
+  }
 
-    .img-card img {
-        width: 100%;
-        height: 100%;
-    }
+  .img-card img {
+    width: 100%;
+    height: 100%;
+  }
 
-    .img-card .operate {
-        width: 100%;
-        font-size: 20px;
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        background-color: #f4f5f6;
-        height: 30px;
-    }
+  .img-card .operate {
+    width: 100%;
+    font-size: 20px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background-color: #f4f5f6;
+    height: 30px;
+  }
 </style>
