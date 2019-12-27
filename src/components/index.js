4fdc31e1e0703e52eddc7457bc84c9112de1b526
xxd <826795224@qq.com>
2019-12-27 15:40:00 +0800

    发布文章-注册富文本编辑器并使用

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,15 @@
 import layoutAside from './home/layout-aside.vue'
 import layoutHeader from './home/layout-header.vue'
 import BreadCrumb from './common/bread-crumb.vue'
+import { quillEditor } from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside) // 全局注册
     Vue.component('layout-header', layoutHeader) // 全局注册
     Vue.component('bread-crumb', BreadCrumb) // 全局注册面包屑组件
+    Vue.component('quill-editor', quillEditor) // 全局注册富文本编辑器
   }
 }
