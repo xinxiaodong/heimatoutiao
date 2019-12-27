b9a99e6c39b8c9402db12477ad33c1cd35b54d17
xxd <826795224@qq.com>
2019-12-27 17:18:02 +0800

    发布文章-封面功能设计

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,36 +1,35 @@
 <template>
-    <el-card>
-        <bread-crumb slot="header">
-            <template slot="title">发布文章</template>
-        </bread-crumb>
-        <!-- 表单 -->
-        <el-form ref="publishForm" :model="formData" :rules="publishRules" style="margin-left: 50px"
-            label-width="100px">
-            <el-form-item prop="title" label="标题">
-                <el-input v-model="formData.title" style="width: 60%"></el-input>
-            </el-form-item>
-            <el-form-item prop="content" label="内容">
-                <quill-editor style="height: 400px;" v-model="formData.content" type="textarea" :rows="4"></quill-editor>
-            </el-form-item>
-            <el-form-item prop="type" label="封面" style="margin-top: 100px">
-                <el-radio-group v-model="formData.cover.type">
-                    <el-radio :label="1">单图</el-radio>
-                    <el-radio :label="3">三图</el-radio>
-                    <el-radio :label="0">无图</el-radio>
-                    <el-radio :label="-1">自动</el-radio>
-                </el-radio-group>
-            </el-form-item>
-            <el-form-item prop="channel_id" label="频道">
-                <el-select v-model="formData.channel_id">
-                    <el-option v-for="item in channels" :key="item.id" :value="item.id" :label="item.name"></el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item>
-                <el-button @click="publishArticle()" type="primary">发布</el-button>
-                <el-button @click="publishArticle(true)">存入草稿</el-button>
-            </el-form-item>
-        </el-form>
-    </el-card>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">发布文章</template>
+    </bread-crumb>
+    <!-- 表单 -->
+    <el-form ref="publishForm" :model="formData" :rules="publishRules" style="margin-left: 50px" label-width="100px">
+      <el-form-item prop="title" label="标题">
+        <el-input v-model="formData.title" style="width: 60%"></el-input>
+      </el-form-item>
+      <el-form-item prop="content" label="内容">
+        <quill-editor style="height: 400px;" v-model="formData.content" type="textarea" :rows="4"></quill-editor>
+      </el-form-item>
+      <el-form-item prop="type" label="封面" style="margin-top: 100px">
+        <el-radio-group  v-model="formData.cover.type">
+          <el-radio :label="1">单图</el-radio>
+          <el-radio :label="3">三图</el-radio>
+          <el-radio :label="0">无图</el-radio>
+          <el-radio :label="-1">自动</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item prop="channel_id" label="频道">
+        <el-select v-model="formData.channel_id">
+          <el-option v-for="item in channels" :key="item.id" :value="item.id" :label="item.name"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="publishArticle()" type="primary">发布</el-button>
+        <el-button @click="publishArticle(true)">存入草稿</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 <script>
@@ -61,60 +60,71 @@ export default {
     $route: function (to, from) {
       if (Object.keys(to.params).length) {
         //  有参数  => 修改
+        this.getArticleById(to.params.articleId) // 重新拉取数据
       } else {
         // 没有参数  => 发布
         this.formData = // 没有参数  => 发布
-                        this.formData = {
-                          title: '', // 标题
-                          content: '', // 文章内容
-                          cover: {
-                            type: 0, //   封面类型 -1:自动，0-无图，1-1张，3-3张
-                            images: [] // 存储的图片的地址
-                          }
-                        }
+            this.formData = {
+              title: '', // 标题
+              content: '', // 文章内容
+              cover: {
+                type: 0, //   封面类型 -1:自动，0-无图，1-1张，3-3张
+                images: [] // 存储的图片的地址
+              }
+            }
       }
-    }
-  },
-  methods: {
-    // 获取频道
-    getChannels () {
-      this.$axios({
-        url: '/channels'
-      }).then(result => {
-        this.channels = result.data.channels
-      })
     },
-    // 发布文章
-    publishArticle (draft) {
-      this.$refs.publishForm.validate((isOK) => {
-        if (isOK) {
-          let { articleId } = this.$route.params // 回去动态路由参数 articleId已经是字符串
-          this.$axios({
-            method: articleId ? 'put' : 'post',
-            url: articleId ? `/articles/${articleId}` : `/articles`,
-            params: { draft }, // query参数
-            data: this.formData
-          }).then(result => {
-            this.$router.push('/home/articles') // 回到内容列表
-          })
-        }
-      })
+    //   // 监控嵌套对象中的值
+    'formData.cover.type': function () {
+      if (this.formData.cover.type === 0 | this.formData.cover.type === -1) {
+        this.formData.cover.images = []
+      } else if (this.formData.cover.type === 1) {
+        this.formData.cover.images = [''] // 单图模式
+      } else if (this.formData.cover.type === 3) {
+        this.formData.cover.images = ['', '', ''] // 三图模式
+      }
     },
-    // 获取文章详情通过id
-    getArticleById (articleId) {
-      this.$axios({
-        url: `/articles/${articleId}`
-      }).then(result => {
-        this.formData = result.data // 将指定文章数据给data数据
-      })
-    }
-  },
-  created () {
-    this.getChannels() // 获取频道数据
-    // 获取id 判断有无id 有id就是修改 没id就是发布
-    let{ articleId } = this.$route.params // 回去动态路由参数
-    if (articleId) {
-      articleId && this.getArticleById(articleId) // 获取文章数据
+    methods: {
+      // 获取频道
+      getChannels () {
+        this.$axios({
+          url: '/channels'
+        }).then(result => {
+          this.channels = result.data.channels
+        })
+      },
+      // 发布文章
+      publishArticle (draft) {
+        this.$refs.publishForm.validate((isOK) => {
+          if (isOK) {
+            let { articleId } = this.$route.params // 回去动态路由参数 articleId已经是字符串
+            this.$axios({
+              method: articleId ? 'put' : 'post',
+              url: articleId ? `/articles/${articleId}` : `/articles`,
+              params: { draft }, // query参数
+              data: this.formData
+            }).then(result => {
+              this.$router.push('/home/articles') // 回到内容列表
+            })
+          }
+        })
+      },
+      // 获取文章详情通过id
+      getArticleById (articleId) {
+        this.$axios({
+          url: `/articles/${articleId}`
+        }).then(result => {
+          this.formData = result.data // 将指定文章数据给data数据
+        })
+      }
+    },
+    created () {
+      this.getChannels() // 获取频道数据
+      // 获取id 判断有无id 有id就是修改 没id就是发布
+      let { articleId } = this.$route.params // 回去动态路由参数
+      if (articleId) {
+        articleId && this.getArticleById(articleId) // 获取文章数据
+      }
     }
   }
 }
