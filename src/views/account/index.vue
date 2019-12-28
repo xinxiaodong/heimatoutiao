2c71356bc1a74cfd93da4d67ab6e1d21f7d75456
xxd <826795224@qq.com>
2019-12-28 17:33:05 +0800

    账户信息-页面结构

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,14 +1,45 @@
 <template>
-<el-card>
-    <bread-crumb slot="header">
-    <template slot="title">账户信息</template>
-</bread-crumb>
-</el-card>
+    <el-card>
+        <bread-crumb slot="header">
+            <template slot="title">账户信息</template>
+        </bread-crumb>
+        <!-- 放置上传组件 -->
+        <el-upload class="head-upload" action="" :show-file-list="false">
+            <img src="../../assets/img/313162.jpg" alt="">
+        </el-upload>
+        <!-- 放置组件 -->
+        <el-form style="margin-left: 100px" label-width="100px">
+            <el-form-item label="用户名">
+                <el-input style="width: 30%"></el-input>
+            </el-form-item>
+            <el-form-item label="简介">
+                <el-input style="width: 30%"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱">
+                <el-input style="width: 30%"></el-input>
+            </el-form-item>
+            <el-form-item label="手机号">
+                <el-input style="width: 30%"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary">保存信息</el-button>
+            </el-form-item>
+        </el-form>
+    </el-card>
 </template>
 
 <script>
 export default {
 }
 </script>
-<style>
+<style scoped>
+     .head-upload{
+         position: absolute;
+         right:300px;
+     }
+    .head-upload img {
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+    }
 </style>
