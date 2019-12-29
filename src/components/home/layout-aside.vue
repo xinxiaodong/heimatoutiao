a86fba7d1e1fdf00f8d79859336f67751f9a3941
xxd <826795224@qq.com>
2019-12-29 15:12:05 +0800

    左侧导航栏的折叠展开

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -2,51 +2,74 @@
     <!-- 左侧导航 -->
     <div class="layout-aside">
         <div class="title">
-            <img src="../../assets/img/logo_admin.png" alt="">
+            <img :src="collaspse ? smallImg : bigImg" alt="">
         </div>
-    <el-menu router style="width: 231px;" background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
-    <el-menu-item index="/home">
-        首页
-    </el-menu-item>
-    <!-- 二级导航 -->
-    <el-submenu index="1">
-        <!-- 具名插槽 -->
-        <span slot='title'>内容管理</span>
-        <!-- 放置 el-menu-item -->
-        <el-menu-item index="/home/publish">发布文章</el-menu-item>
-        <el-menu-item index="/home/articles">内容列表</el-menu-item>
-        <el-menu-item index="/home/comment">评论列表</el-menu-item>
-        <el-menu-item index="/home/material">素材管理</el-menu-item>
-    </el-submenu>
-    <el-submenu index="2">
-        <span slot='title'>粉丝管理</span>
-        <el-menu-item index="/home/picture">图文数据</el-menu-item>
-        <el-menu-item index="/home/fansinfo">粉丝画像</el-menu-item>
-        <el-menu-item index="/home/fanslife">粉丝概况</el-menu-item>
-        <el-menu-item index="/home/fanslist">粉丝列表</el-menu-item>
-    </el-submenu>
-    <el-menu-item index="/home/account">
-        账户信息
-    </el-menu-item>
-    </el-menu>
+        <el-menu :collapse="collaspse" router :style="{width: collaspse ? '60px': '230px'}" background-color="#353b4e"
+            text-color="#adafb5" active-text-color="#ffd04b">
+            <el-menu-item index="/home">
+                <i class='el-icon-s-home'></i>
+                <span>首页</span>
+            </el-menu-item>
+            <!-- 二级导航 -->
+            <el-submenu index="1">
+                <!-- 具名插槽 -->
+                <template slot='title'>
+                    <i class='el-icon-s-order'></i>
+                    <span>内容管理</span>
+                </template>
+
+                <!-- 放置 el-menu-item -->
+                <el-menu-item index="/home/publish">发布文章</el-menu-item>
+                <el-menu-item index="/home/articles">内容列表</el-menu-item>
+                <el-menu-item index="/home/comment">评论列表</el-menu-item>
+                <el-menu-item index="/home/material">素材管理</el-menu-item>
+            </el-submenu>
+            <el-submenu index="2">
+                <template slot='title'>
+                    <i class='el-icon-s-flag'></i>
+                    <span>粉丝管理</span>
+                </template>
+                <el-menu-item index="/home/picture">图文数据</el-menu-item>
+                <el-menu-item index="/home/fansinfo">粉丝画像</el-menu-item>
+                <el-menu-item index="/home/fanslife">粉丝概况</el-menu-item>
+                <el-menu-item index="/home/fanslist">粉丝列表</el-menu-item>
+            </el-submenu>
+            <el-menu-item index="/home/account">
+                <i class='el-icon-s-custom'></i>
+                <span>账户信息</span>
+            </el-menu-item>
+        </el-menu>
     </div>
 </template>
 
 <script>
 export default {
+  props: ['collaspse'], // 接收传递过来的折叠值
+  data () {
+    return {
+      bigImg: require('../../assets/img/logo_admin.png'),
+      smallImg: require('../../assets/img/logo_admin.png')
+    }
+  }
 }
 </script>
 <style scoped>
-    .layout-aside{
+    .layout-aside {
         overflow: hidden;
-        width: 230px;
+        /* width: 230px; */
     }
-   .title{
-   text-align: center;
-   background-color: #2e2f32 ;
-   padding: 10px 0;
+
+    .layout-aside .el-menu {
+        border-right: none;
     }
-    img{
+
+    .title {
+        text-align: center;
+        background-color: #2e2f32;
+        padding: 10px 0;
+    }
+
+    img {
         height: 35px;
     }
-   </style>
+</style>
--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -1,7 +1,8 @@
 <template>
   <el-row class='layout-header' type="flex" align="middle" justify="space-between">
     <el-col class="left" :sapn="6">
-      <i class='el-icon-s-unfold'></i>
+      <!-- 左侧图标 -->
+      <i @click="collaspseOrOpen" :class="{'el-icon-s-unfold': collaspse,'el-icon-s-fold': !collaspse}"></i>
       <span class='title'>江苏传智播客教育科技股份有限公司</span>
     </el-col>
     <el-col class="right" :sapn="4">
@@ -28,6 +29,7 @@ import eventBus from '../../utils/eventBus.js' // 引入公共实例
 export default {
   data () {
     return {
+      collaspse: false, // 是否折叠
       userInfo: {}, // 用户信息
       defaultImg: require('../../assets/img/313400.jpg') // 先把地址转换称变量
     }
@@ -42,6 +44,12 @@ export default {
     })
   },
   methods: {
+    collaspseOrOpen () {
+      // 直接取反
+      this.collaspse = !this.collaspse // 直接返回
+      // 通知要改变宽度的组件
+      eventBus.$emit('changeCollapse') // 改变折叠状态
+    },
     getUserInfo () {
       this.$axios({
         url: '/user/profile'
@@ -76,7 +84,9 @@ export default {
     color: #2c3e50;
     font-size: 16px;
   }
-
+  .left i{
+    font-size: 22px;
+  }
   .right img {
     width: 40px;
     height: 40px;
